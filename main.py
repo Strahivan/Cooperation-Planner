f5271fbc4956fb5c8261a0a3016bc53a651d439b
mx <michelu1989@gmail.com>
2018-09-26 02:05:17 +0200

    Little changes to get database from subfolder

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-from flaskext.mysql import MySQL
+import database.queries as query
+
 app = Flask(__name__)
+
+app.config.from_pyfile('database.cfg')
 
 @app.route('/')
 def hello_world():
-# this needs to update in realtime too and deliver some parameters
-    return render_template('index.html', resultsToDisplay = data)
+    # this needs to update in realtime too and deliver some parameters
+    return render_template('index.html', resultsToDisplay=query.get_url())
+
 
 @app.route('/input_File')
 # this is a dynamic AJAX-request without a new request
@@ -13,26 +17,11 @@ def inputCsvFile():
     print "i worked"
     return "nothing"
 
-
-mysql = MySQL()
- 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'iAmGod!4Sure'
-app.config['MYSQL_DATABASE_DB'] = 'urlinput'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
-
-conn = mysql.connect()
-cur = conn.cursor()
-cur.execute('SELECT * FROM url')
-data = cur.fetchall()
-
-# hier muss dann noch aus dem datahandler die entsprechenden Methoden aufgerufen werden, die die datenbank betreffen. 
+# hier muss dann noch aus dem datahandler die entsprechenden Methoden aufgerufen werden, die die datenbank betreffen.
 
 # Ausserdem muss in der View angepasst werden also der helloworld Methode dass Parameter als Liste in diesem Fall aus der DB mitgegeben und angezeigt werden
 
-# Somit muss auch HTML angepasst werden (also die index.html) 
+# Somit muss auch HTML angepasst werden (also die index.html)
 
 # Ausserdem solltest du noch ein MYSQL-Skript schreiben, dass bei installation der App ausgefuehrt wird)
 
